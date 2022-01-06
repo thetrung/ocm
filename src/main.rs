2dfc3a4305bb86d007e0ac08e21031392bc4adee
deulamco <deulamco@iMac-deulamco.local>
2022-01-07 00:27:55 +0700

    working analyzer, show up profitable trades with fees calculated

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate binance;
 
-use std::{collections::HashMap, thread::{self, JoinHandle}, time};
+use std::{thread::{self, JoinHandle}, time};
 
 use colored::*;
 use configparser::ini::Ini;
@@ -8,94 +8,168 @@ use configparser::ini::Ini;
 use binance::{api::*, model::{Prices, SymbolPrice}};
 use binance::market::*;
 use binance::account::*;
-
-struct Hunter {
-    symbol: String,
-    is_riding: bool,
-    heart_beat: i32, // ms
-    gain: f64,       // %
-    gain_min: f64,   // %
-    gain_max: f64,   // %
-}
-
-struct Config {
-    // keys
-    api_key: String,
-    secret_key: String,
-    // thread interval
-    init_heart_beat: i32,
-    // bot config
-    fix_gain_step: f64,
-    init_gain_min: f64,
-    init_gain_max: f64
-}
-
-const one_second: time::Duration = time::Duration::from_millis(1000);
-const two_second: time::Duration = time::Duration::from_millis(2500);
-
+// NOTE:
+//
+// we will use this to finish the ring with 3 parts : 
+// BUSD >> ETHBUSD > ETHBNB > BNBBUSD >> BUSD
+//
+// which mean each ring need to compose like : 
+// 1. symbol + BUSD 
+// 2. symbol + BNB
+// then combine : 1 > 2 > 1
+//
 fn main() {
-    // caching to extend live of data
-    let mut data_cache: Vec<SymbolPrice> = vec![];
-    let mut config = Ini::new();
-    config.load("config.toml");
-    let market = get_market(&mut config);
-    // let symbols = symbol_scan(&market, &mut data_cache);
+    // CONSTANT
+    const DELAY_INIT: time::Duration = time::Duration::from_millis(100);
 
-    // Order book at depth 500
-    match market.get_depth("BNBBUSD") {
-        Ok(answer) => println!("{:?}", answer),
-        Err(e) => println!("Error: {}", e),
+    //
+    // CONFIG 
+    //
+    let mut config = Ini::new();
+    let _ = config.load("config.toml");
+    let market = get_market(&mut config);
+    //
+    // BUILD RINGS
+    //
+    let data_ignored_symbols = &config.get("symbols","ignored").unwrap();
+    let data_bridge_symbols = &config.get("symbols","bridges").unwrap();
+    //
+    // Transform data :
+    //
+    let ignored_symbols:Vec<&str> = data_ignored_symbols.split(',').collect();
+    let bridges_symbols:Vec<&str> = data_bridge_symbols.split(',').collect();
+    let mut data_cache:Vec<SymbolPrice> = vec![];
+    //
+    // LOG-TEST
+    //
+    // println!("{}-{}",data_ignored_symbols, ignored_symbols.len());
+    // println!("{}-{}",data_bridge_symbols, bridges_symbols.len());
+    // for bridge in &bridges_symbols {
+    //     println!("{}", bridge);
+    // }
+    // for ignore in &ignored_symbols {
+    //     println!("{}", ignore);
+    // }
+    
+    let rings = symbol_scan(&market, &ignored_symbols, &bridges_symbols, &mut data_cache);
+
+    //
+    // THREADS
+    //
+    let mut hunter_pool:Vec<JoinHandle<(Option<String>)>> = vec![];
+
+    for ring in rings {
+        thread::sleep(DELAY_INIT);
+        let thread_ring = ring.clone();
+        let thread = thread::spawn(|| { order_chain(thread_ring) });
+        hunter_pool.push(thread);
     }
 
-    // Test 
-    // let symbols = vec![
-    //     "SFPUSDT", 
-    //     "DODOUSDT", 
-    //     "FIROUSDT", 
-    //     "SANDUSDT", 
-    //     "PERLUSDT"
-    //     ]; // no gas
+    //
+    // later, we may use signal to trigger this
+    //
+    println!("> Last Block before joining all threads.. <");
+
+    //
+    // Joining all
+    //
+    for hunter in hunter_pool{
+        match hunter.join().unwrap() {
+            Some(name) => {
+                println!("\"{}\"",name);
+                //
+                // Plan :
+                // We may rank the most profit one from each round,
+                // then execute the order ring as fast as possible.
+                // benchmark the delay + order book.
+                //
+                // Analyze > Ranking > Execute > Evaluate > Sleep > Repeat
+                //
+            },
+            None => {
+
+            }
+        }
+    }
+
+    // ending
+    println!("Ended all threads.");
+
+    // Order book at depth 500
+    // match market.get_depth("BNBBUSD") {
+    //     Ok(answer) => println!("{:?}", answer),
+    //     Err(e) => println!("Error: {}", e),
+    // }
+}
+
+fn order_chain(ring: [String;3]) -> Option<String> {
+    //
+    // config file loading
+    //
+    let mut config = Ini::new();
+    let _ = config.load("config.toml");
+    let market = get_market(&mut config);
+    //
+    //  1. Get average price of each symbol
+    //  2. Convert the ring total price into profit
+    //  3. Compare and Execute Order Chains
+    //
+    let ring_name = ring[0].clone().replace("BUSD","");
+    let mut ring_prices = vec![];
+    //
+    // collect prices :
+    //
+    for sym in ring {
+        match market.get_price(sym) {
+            Ok(answer) => {
+                // println!("{:?}", answer);
+                ring_prices.push(answer.price);
+            },
+            Err(e) => println!("Error: {}", e),
+        }
+    }
+    // calculate if it's profitable :
+    let invest = 5000.0;
+    let binance_fees = 0.1; // as 0.1 ~ 0.0750% 
+    let warning_ratio = 20.0; // 1:20 ratio
+    let fees = 1.0 - (binance_fees / 100.0); 
+    let sum = ( invest / ring_prices[0] ) * ring_prices[1] * ring_prices[2];
+    let profit = (sum * fees * fees * fees ) - invest;
+    //
+    // INVALID
+    //
+    if profit > invest / warning_ratio { 
+        println!("{}\n- FINISH -\n\n","WARNING: REMOVE THIS PAIR".red());
+        return None;
+        // return Some(ring_name); // in case, we need to ignore;
+    }
+    //
+    // OK
+    //
+    if profit > 0.0 {
+        println!("ring: {} {} {}",ring_prices[0], ring_prices[1], ring_prices[2]);
+        println!("{}={}\n\n", &ring_name, (&profit).to_string().green());
+        return Some(ring_name);
+    }
     
-    // let mut hunter_pool:Vec<JoinHandle<()>> = vec![];
-
-    // for symbol in symbols {
-    //     thread::sleep(one_second);
-    //     let sym_str = String::from(symbol);
-    //     let thread = thread::spawn(|| { whale_scan( sym_str); });
-    //     hunter_pool.push(thread);
-    // }
-
-    // //
-    // // later, we may use signal to trigger this
-    // //
-    // println!("> Last Block before joining all threads.. <");
-
-    // //
-    // // Joining all
-    // //
-    // for hunter in hunter_pool{
-    //     hunter.join().unwrap(); 
-    // }
-
-    // // ending
-    // println!("Ended all threads.");
+    return None;
 }
 
 // 'a is to fix the damn "named lifetime parameter" to indicate same data flow
-fn symbol_scan<'a>(market: &Market, data_cache: &'a mut Vec<SymbolPrice>) -> Vec<&'a str> {
+fn symbol_scan<'a>(
+    market: &Market,
+    symbols_except: &Vec<&str>, 
+    symbols_bridge: &Vec<&str>,
+    data_cache: &'a mut Vec<SymbolPrice>) -> Vec<[String; 3]> {
 
-    // TODO: use
-    let mut symmap:HashMap<&str,f64> = HashMap::new();
-    let mut symbols_BTC:Vec<&str> = vec![];
-    let mut symbols:Vec<&str> = vec![]; // init
-    let symbols_except = vec![   
-        "USDSUSDT",
-        "USDCUSDT",      
-        "USDSBUSDT"
-    ];
     //
-    // Fetching all prices of symbols on Binance..
-    // may call this once per second, and calculate average ourselves.
+    // INIT
+    //
+    let mut symbols_BUSD:Vec<&str> = vec![];
+    let mut symbols_BNB:Vec<&str> = vec![]; 
+
+    //
+    // Fetching all symbols from Binance.
     //
     match market.get_all_prices() {
         Ok(answer) => {
@@ -106,47 +180,62 @@ fn symbol_scan<'a>(market: &Market, data_cache: &'a mut Vec<SymbolPrice>) -> Vec
                     // caching
                     *data_cache = data.clone();
                     
-                    for item in data_cache {
+                    for symbol_price in data_cache {
                         //
                         // Filter stuffs here
                         // TODO: use HashMap to add also prices
-                        let i = item.symbol.as_str();
-                        if i.ends_with("USDT") && !symbols_except.contains(&i) { symbols.push(i); }
-                        if i.ends_with("BTC") { symbols_BTC.push(i); }
+                        let key = symbol_price.symbol.as_str();
+                        let mut bridge_iter = symbols_bridge.iter();
+                        // println!("key: {}", key);
+                        if bridge_iter.any(|bridge_sym| {
+                            // println!("item: {} vs. {}", &bridge_sym, key);
+                            key.contains(bridge_sym) 
+                        } && !symbols_except.contains(&key)) {
+                            // Passed :
+                            if key.ends_with("BUSD") {
+                                symbols_BUSD.push(key); 
+                                // println!(">> BUSD: {}", key);
+                            }
+                            else if key.ends_with("BNB"){
+                                symbols_BNB.push(key); 
+                                // println!(">> sym: {}", key);
+                            } 
+                        } else {
+                            // println!("ignored: {}", key);
+                        }
                     }
                 }
             }
         },
-        Err(e) => println!("Error with data_cache = {:2}\n{:1}", e, &data_cache.len()),
+        Err(e) => println!("Error with data_cache = {:2}\n", e),
     }
 
-    &symbols.sort();
-    for symbol in symbols.clone() { 
-        println!("sym {:<16}", &symbol); 
-    }
-    println!("Total USDT symbols is {}\n", symbols.len());
-    //
-    // TODO:
-    // Merge this filter into above loop, after got all prices,
-    // we may later call this a lot instead of each symbol thread 
-    // calling its own average_price and current_price.
-    //
-    for symbol in &symbols {
-        let i = &symbol[0..(symbol.len()-4)];
-        //
-        // excluding leverage symbols & pairs without BTC
-        //
-        if !&i.contains("UP") && !&i.contains("DOWN") {
-            let possible_sym = i.to_owned() + "BTC";
-            if symbols_BTC.contains(&possible_sym.as_str()) {
-                symmap.insert(&symbol, 0.0);
-                println!("i = {}", &symbol);
-            }
+    println!("Total bridge-able BNB symbols is {}\n", symbols_BNB.len());
+    println!("Total bridge-able BUSD symbols is {}\n", symbols_BUSD.len());
+
+    let mut symbols_rings = vec![];
+
+    for sym in symbols_BUSD {
+        let name = String::from(sym.strip_suffix("BUSD").unwrap());
+        let bridge = [name.clone(), "BNB".to_string()].join("");
+        if symbols_BNB.contains(&bridge.as_str()) {
+            println!("converting {} to {} for {}", &sym, &name.as_str(), &bridge.as_str());
+            //
+            // Note:
+            // Remember to clone/new String to copy/concat around.
+            //
+            symbols_rings.push([
+                String::from(sym), 
+                bridge.clone(),     // clone bridge.
+                "BNBBUSD".to_string()
+            ]);
         }
+        println!("{}-{}-{}", sym, bridge, "BNBBUSD"); // name+bridge moved here
     }
-    println!("Total BTC pairs is {}\n", &symmap.len());
 
-    return symbols;
+    println!("Total symbol rings is {}\n", symbols_rings.len());
+
+    return symbols_rings;
 }
 
 fn get_str<'a>(config: &mut Ini, key: &str) -> Option<String> {
@@ -160,158 +249,58 @@ fn get_market(config: &mut Ini) -> Market {
     return market;
 }
 
-fn whale_scan(symbol: String)
-{            
-    // fixed stuffs
-    let mut epoch = 0;
-    let _symbol = symbol.as_str();
-    // config file loading
-    let mut config = Ini::new();
-    let map = config.load("config.toml");
-    match map {
-        Ok(_) => {
-            // let account: Account = Binance::new(api_key.clone(), secret_key.clone());
-            let market = get_market(&mut config);
-            loop {
-                //
-                // TODO: Replace with main thread average prices
-                //
-                match &market.get_average_price(_symbol) {
-                    Ok(answer) => { 
-                        // increase cycle 
-                        epoch += 1; 
-                        //
-                        // indicate we are riding whale or not 
-                        let mut is_whale_riding = false;
-                        //
-                        // Check Average price first
-                        let _average = answer.price;
-                        //
-                        // compute diff
-                        let _diff = compute_change(&market, &_symbol, _average, epoch);
-                        //
-                        // processing "changes" when we already know it's "Rise" of "Fall"
-                        //
-                        decision_making(_diff,  &mut is_whale_riding, &_symbol);
-                        //
-                        // end line
-                        println!();
-                    },
-                    Err(e) => println!("Error: {}", e),
-                }
-                // sleep for a while
-                thread::sleep(two_second);
-            }
-        },
-        Err(_) => println!("{}","can't load config file.".red())
-    }
-}
+// fn buy_symbol_with_btc<S>(market: Market, account: Account) 
+// where S: Into<String> 
+// {
+//     println!("Which symbol to buy ? ");
 
-fn buy_symbol_with_btc<S>(market: Market, account: Account) 
-where S: Into<String> 
-{
-    println!("Which symbol to buy ? ");
+//     let mut symbol:String = String::new();
+//     std::io::stdin()
+//         .read_line(&mut symbol)
+//         .ok()
+//         .expect("invalid symbol !");
 
-    let mut symbol:String = String::new();
-    std::io::stdin()
-        .read_line(&mut symbol)
-        .ok()
-        .expect("invalid symbol !");
+//     // convert to String to borrow later
+//     let _symbol:String = symbol.into();
 
-    // convert to String to borrow later
-    let _symbol:String = symbol.into();
+//     // Latest price for ONE symbol
+//     match market.get_price(&_symbol) {
+//         Ok(answer) => {
+//             println!("\n- {}: {}", answer.symbol, answer.price);
+//             let current_price = answer.price;
 
-    // Latest price for ONE symbol
-    match market.get_price(&_symbol) {
-        Ok(answer) => {
-            println!("\n- {}: {}", answer.symbol, answer.price);
-            let current_price = answer.price;
+//             // get all BTC 1st 
+//             match account.get_balance("BTC") {
+//                 Ok(answer) => {
+//                     println!("- BTC free: {}", answer.free);
+//                     // "balances": [
+//                     // {
+//                     //     "asset": "BTC",
+//                     //     "free": "4723846.89208129",
+//                     //     "locked": "0.00000000",
+               // },
+//                     let available_btc:f64 = answer.free.parse().unwrap();
+//                     let qty = &available_btc / &current_price;
+//                     //
+//                     // we convert all current BTC into the next coin:
+//                     //
 
-            // get all BTC 1st 
-            match account.get_balance("BTC") {
-                Ok(answer) => {
-                    println!("- BTC free: {}", answer.free);
-                    // "balances": [
-                    // {
-                    //     "asset": "BTC",
-                    //     "free": "4723846.89208129",
-                    //     "locked": "0.00000000"
-                    // },
-                    let available_btc:f64 = answer.free.parse().unwrap();
-                    let qty = &available_btc / &current_price;
-                    //
-                    // we convert all current BTC into the next coin:
-                    //
+//                     println!("- market_buy {} {}", qty ,_symbol);
 
-                    println!("- market_buy {} {}", qty ,_symbol);
+//                     // buy all with btc 
+//                     match account.market_buy(&_symbol, qty) {
+//                         Ok(answer) => {
+//                             println!("- success => {:?}\n", answer)
+//                         },
+//                         Err(e) => println!("- ERROR: \n{:?}", e),
+//                     }
+//                 },
+//                 Err(e) => println!("Error: {:?}", e),
+//             }
+//         },
+//         Err(e) => println!("Error: {:?}", e),
+//     }
 
-                    // buy all with btc 
-                    match account.market_buy(&_symbol, qty) {
-                        Ok(answer) => {
-                            println!("- success => {:?}\n", answer)
-                        },
-                        Err(e) => println!("- ERROR: \n{:?}", e),
-                    }
-                },
-                Err(e) => println!("Error: {:?}", e),
-            }
-        },
-        Err(e) => println!("Error: {:?}", e),
-    }
+//     println!("\n");
+// }
 
-    println!("\n");
-}
-
-fn compute_change(market: &Market, symbol: &&str, average:f64, epoch: i32) -> f64
-{
-    //
-    // Compare to latest price 
-    //
-    let mut _diff = 0.0;
-    fn detect_changes(changes: f64) -> String {
-        return String::from(if changes > 1.0 {"Gain"} else {"Loss"});
-    }
-    match market.get_price(*symbol) {
-        Ok(answer) => {
-            // calculate stuffs 
-            let _changes = answer.price / average;
-            _diff = (_changes-1.0) * 100.0;
-            // log
-            let log = format!(
-                "[epoch #{}]: {:<10}: average = {:.2} | {}: {:.2}%", 
-                epoch, symbol, average, detect_changes(_changes), _diff);
-            print!("{}", if _diff > 0.0 {log.green()} else {log.red()});
-        },
-        Err(e) => println!("Error: {}", e),
-    }
-    return _diff;  // return
-}
-
-fn decision_making(_diff:f64, is_whale_riding: &mut bool, symbol: &&str){
-    //
-    // Define BUY RATIO here
-    //
-    let ratio_whale_pump:f64 = 2.0; // percent
-
-    if _diff > ratio_whale_pump {
-        //
-        // BUY
-        //
-        print!("{}", format!("=> WHALE DETECTED => BUY XXX {}", symbol).white().bold());
-        *is_whale_riding = true;
-
-    } 
-    else {
-        if *is_whale_riding {
-            //
-            // SELL
-            //
-            print!("{}",format!(" => WHALE DUMP => SELL ALL {}", symbol).red());
-        } 
-        else {
-            //
-            // DOING NOTHING 
-            //
-        }
-    }
-}
