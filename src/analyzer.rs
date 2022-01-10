e69f37fe242a08cfd5fac8ffccb6d438c564269f
deulamco <deulamco@iMac-deulamco.local>
2022-01-10 14:33:30 +0700

    clean up, refactor main/analyzer code

--- src/analyzer.rs
+++ src/analyzer.rs
@@ -28,18 +28,21 @@ use binance::market::*;
 const SYMBOL_CACHE_FILE:&str = "symbols.cache";
 
 // 'a is to fix the damn "named lifetime parameter" to indicate same data flow
-pub fn symbol_discovery<'a>(
-    market: &Market,
-    symbols_except: &Vec<&str>, 
-    symbols_bridge: &Vec<&str>,
-    data_cache: &'a mut Vec<SymbolPrice>) -> HashMap<String, Vec<String>>{
+pub fn symbol_discovery<'a>(config: &Ini, market: &Market) -> HashMap<String, Vec<String>>{
+    //
+    // LOAD CONFIGS
+    //
+    let _key_bridge = &config.get("symbols","bridges").unwrap();
+    let _key_ignored = &config.get("symbols","ignored").unwrap();
+    let bridge_symbols:Vec<&str> = _key_bridge.split(',').collect();
+    let ignored_symbols:Vec<&str> = _key_ignored.split(',').collect();
     //
     // INIT CACHES
     //
+    let mut data_cache:Vec<SymbolPrice> = vec![];
     let mut symbols_stablecoin:Vec<&str> = vec![];
     let mut symbols_with_bridge:Vec<&str> = vec![]; 
     let mut symbols_rings: HashMap<String, Vec<String>> = HashMap::new();
-
     //
     // FIND & LOAD CACHED FILE
     //
@@ -51,15 +54,8 @@ pub fn symbol_discovery<'a>(
                 for discovered in sym_map {
                     println!("> loading {} rings...", discovered.1.len());
                     for symbol_ring in discovered.1 {
-                        //
-                        // Build Ring :
-                        //
-                        // print!("\n{}: ", symbol_ring.0.to_uppercase());
-                        let ring:Vec<String> = symbol_ring.1.unwrap().split(",").map(|s| s.to_string()).collect();
-                        //
-                        // Insert into map :
-                        //
-                        // for sym in &ring { print!("{} ", sym)}
+                        let ring:Vec<String> = 
+                        symbol_ring.1.unwrap().split(",").map(|s| s.to_string()).collect();
                         symbols_rings.insert( symbol_ring.0.to_uppercase(), ring);
                     }
                 }
@@ -76,62 +72,56 @@ pub fn symbol_discovery<'a>(
                     match answer {
                         // need to match the exact enum
                         Prices::AllPrices(data) => {
-                            // caching
-                            *data_cache = data.clone();
+                            //
+                            // ground caching for other hashes 
+                            //
+                            data_cache = data.clone();
                             
-                            for symbol_price in data_cache {
-                                //
-                                // Filter stuffs here
-                                // TODO: use HashMap to add also prices
+                            for symbol_price in &data_cache {
                                 let key = symbol_price.symbol.as_str();
-                                let mut bridge_iter = symbols_bridge.iter();
-                                // println!("key: {}", key);
-                                if bridge_iter.any(|bridge_sym| {
-                                    // println!("item: {} vs. {}", &bridge_sym, key);
-                                    key.contains(bridge_sym) 
-                                } && !symbols_except.contains(&key)) {
-                                    // Passed :
-                                    if key.ends_with("BUSD") {
-                                        symbols_stablecoin.push(key); 
-                                        // println!(">> BUSD: {}", key);
+                                let mut bridge_iter = bridge_symbols.iter();
+                                //
+                                // Find only stablecoin/bridge pairs :
+                                //
+                                if bridge_iter.any(|b| key.contains(b)) && !ignored_symbols.contains(&key){
+                                    // Stablecoin :
+                                    if key.ends_with(bridge_symbols[0]) {
+                                        symbols_stablecoin.push(&key); 
                                     }
-                                    else if key.ends_with("BNB"){
-                                        symbols_with_bridge.push(key); 
-                                        // println!(">> sym: {}", key);
+                                    // Bridge :
+                                    else if key.ends_with(bridge_symbols[1]){
+                                        symbols_with_bridge.push(&key);
                                     } 
-                                } else {
-                                    // println!("ignored: {}", key);
-                                }
+                                } 
                             }
                         }
                     }
                 },
                 Err(e) => println!("Error with data_cache = {:2}\n", e),
             }
-            println!("- Total bridge-paired symbols is {}", symbols_with_bridge.len());
-            println!("- Total stablecoin-paired symbols is {}", symbols_stablecoin.len());
+            println!("- Total bridge-pairs : {}", symbols_with_bridge.len());
+            println!("- Total stablecoin-pairs : {}", symbols_stablecoin.len());
 
             for sym in symbols_stablecoin {
                 // 1 - stablecoin
                 // 2 - bridge
-                let name = String::from(sym.strip_suffix(symbols_bridge[0]).unwrap());
-                let bridge = [name.clone(), symbols_bridge[1].to_string()].join("");
+                let name = String::from(sym.strip_suffix(bridge_symbols[0]).unwrap());
+                let bridge = [name.clone(), bridge_symbols[1].to_string()].join("");
                 if symbols_with_bridge.contains(&bridge.as_str()) {
                     //
                     // Note:
                     // Remember to clone/new String to copy/concat around.
                     //
                     symbols_rings.insert(String::from(name), vec![
-                        String::from(sym), 
-                        bridge.clone(),     // clone bridge.
-                        "BNBBUSD".to_string()
+                        String::from(sym),                              // sym => XTZ-BUSD,
+                        bridge.clone(),                                 // bridge => XTZ-BNB,
+                        [bridge_symbols[1], bridge_symbols[0]].join("") // bridge-stablecoin => BNB-BUSD
                     ]);
                 }
-                // println!("{}-{}-{}", sym, bridge, "BNBBUSD"); // name+bridge moved here
             }
             println!("- Total symbol rings is {}", symbols_rings.len());
             //
-            // Save all discovered symbols >> cache file
+            // Save all discovered symbols 
             //
             let mut cache_file = Ini::new();
             for sym in &symbols_rings {
@@ -151,24 +141,99 @@ pub fn symbol_discovery<'a>(
     return symbols_rings;
 }
 
+pub fn init_threads(rings: HashMap<String, Vec<String>>){
 
-pub fn order_chain(ring: Vec<String>) -> Option<String> {
+    // CONSTANT
+    const DELAY_INIT: time::Duration = time::Duration::from_millis(100);
+    const DELAY_ROUND: time::Duration = time::Duration::from_millis(1000*5);
+    //
+    // THREADS
+    //
+    let mut hunter_pool:Vec<JoinHandle<(Option<String>)>> = vec![];
+
+    for ring in rings {
+        thread::sleep(DELAY_INIT);
+        let thread_ring = ring.1.clone();
+        let thread = thread::spawn(|| { order_chain(ring.0, thread_ring) });
+        hunter_pool.push(thread);
+    }
+    //
+    // Joining all
+    //
+    let mut round_result = vec![];
+    for hunter in hunter_pool{
+        match hunter.join().unwrap() {
+            Some(name) => {
+                // make a list of ring:
+                round_result.push(name);
+                //
+                // Plan :
+                // We may rank the most profit one from each round,
+                // then execute the order ring as fast as possible.
+                // benchmark the delay + order book.
+                //
+                // Analyze > Ranking > Execute > Evaluate > Sleep > Repeat
+                //
+            },
+            None => {
+
+            }
+        }
+    }
+    thread::sleep(DELAY_ROUND);
+    //
+    // later, we may use signal to trigger this
+    //
+    println!("> Joining all threads.. <");
+    println!("=== This Round result ===");
+
+    for name in round_result {
+        println!("\"{}\"",name);
+    }
+
+    // ending
+    println!("Ended all threads.");
+}
+
+pub fn order_chain(symbol: String, ring: Vec<String>) -> Option<String> {
     //
     // config file loading
     //
     let mut config = Ini::new();
     let _ = config.load("config.toml");
     let market = get_market(&mut config);
-    //
-    //  1. Get average price of each symbol
-    //  2. Convert the ring total price into profit
-    //  3. Compare and Execute Order Chains
-    //
-    let ring_name = ring[0].clone().replace("BUSD","");
+
+    // collect prices vector
     let mut ring_prices = vec![];
     //
     // collect prices :
+    // 
+    // All we need is this tickers to divide into ASK+BID table
+    // to cache prices for each symbol before computing profitable chances.
+    // 
     //
+    // match market.get_all_book_tickers() {
+    //     Ok(answer) => {
+    //         match &answer {
+    //             binance::model::BookTickers::AllBookTickers(all) => {
+    //                 match all {
+    //                     tickers => {
+    //                         for ticker in tickers {
+    //                             println!("{}: ask {} x {} === bid {} x {}", 
+    //                             ticker.symbol,    // Symbols
+    //                             ticker.ask_price, // Sell orders
+    //                             ticker.ask_qty,
+    //                             ticker.bid_price, // Buy orders
+    //                             ticker.bid_qty);
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //         // println!("{:?}", &answer);
+    //     },
+    //     Err(e) => println!("Error: {}", e),
+    // }
     for sym in ring {
         match market.get_price(sym) {
             Ok(answer) => {
@@ -203,11 +268,11 @@ pub fn order_chain(ring: Vec<String>) -> Option<String> {
             ring_prices[0], ring_prices[1], ring_prices[2]).to_string().cyan());
 
         println!("{} = {} / {:.5}%\n\n", 
-            &ring_name.bold(), 
+            &symbol.bold(), 
             (&profit).to_string().green(), 
             ((&profit/&invest)*100.0).to_string().yellow());
 
-        return Some(ring_name);
+        return Some(symbol);
     }
     
     return None;
@@ -217,13 +282,8 @@ pub fn order_chain(ring: Vec<String>) -> Option<String> {
 // UTILITIES
 //
 
-fn get_str<'a>(config: &mut Ini, key: &str) -> Option<String> {
-    let conf = Some(config.get("keys", key).unwrap());
-    return conf;
-}
 pub fn get_market(config: &mut Ini) -> Market {
-    let secret_key = get_str(config, "secret_key");
-    let api_key = get_str(config, "api_key");
-    let market: Market = Binance::new(api_key, secret_key);
-    return market;
+    return Binance::new(
+        Some(config.get("keys", "api_key").unwrap()), 
+        Some(config.get("keys", "secret_key").unwrap()));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,27 @@
 extern crate binance;
 
-use std::{
-    thread::{self, JoinHandle} ,
-    collections::HashMap, 
-    time};
+use std::{ thread::{self, JoinHandle}, time};
 
 use colored::*;
 use configparser::ini::Ini;
 
 use binance::{api::*, model::{Prices, SymbolPrice}};
-use binance::market::*;
 
 mod analyzer;
 // NOTE:
 //
 // 1. Analyze
-// Discover all trade-able symbols and serialize to cache on 1st run.
-// * Could make a filtering phrase by testing run to filter old/unused pairs.
+// [done] Discover all trade-able symbols and serialize to cache on 1st run.
+// [*] Could make a filtering phrase by testing run to filter old/unused pairs.
 //
 // 2. Update Orderbooks
-// Update price by bookTickers to cache + Calculate profit through each ring.
+// [wip] Update price by bookTickers to cache + Calculate profit through each ring.
 // 
 // 3. Execute Order
-// Use Market Buy/Sell as fast as possible when profit is positive.
+// [*] Use Market Buy/Sell as fast as possible when profit is positive.
 // 
 // 4. Prepare for next Block
-// Each block last for 10 seconds to avoid binance warn : 
+// [*] Each block last for 10 seconds to avoid binance warn : 
 // - Update OrderBooks
 // - Calculate profit 
 // - Execute Order
@@ -33,107 +29,19 @@ mod analyzer;
 // - Wait next block
 //
 fn main() {
-    // CONSTANT
-    const DELAY_INIT: time::Duration = time::Duration::from_millis(100);
-    const DELAY_ROUND: time::Duration = time::Duration::from_millis(1000*5);
-
     //
     // CONFIG 
     //
     let mut config = Ini::new();
     let _ = config.load("config.toml");
     let market = analyzer::get_market(&mut config);
-    // 
-    // All we need is this tickers to divide into ASK+BID table
-    // to cache prices for each symbol before computing profitable chances.
-    // 
-    //
-    // match market.get_all_book_tickers() {
-    //     Ok(answer) => {
-    //         match &answer {
-    //             binance::model::BookTickers::AllBookTickers(all) => {
-    //                 match all {
-    //                     tickers => {
-    //                         for ticker in tickers {
-    //                             println!("{}: ask {} x {} === bid {} x {}", 
-    //                             ticker.symbol,    // Symbols
-    //                             ticker.ask_price, // Sell orders
-    //                             ticker.ask_qty,
-    //                             ticker.bid_price, // Buy orders
-    //                             ticker.bid_qty);
-    //                         }
-    //                     }
-    //                 }
-    //             }
-    //         }
-    //         // println!("{:?}", &answer);
-    //     },
-    //     Err(e) => println!("Error: {}", e),
-    // }
-    // return;
     //
     // BUILD RINGS
     //
-    let data_ignored_symbols = &config.get("symbols","ignored").unwrap();
-    let data_bridge_symbols = &config.get("symbols","bridges").unwrap();
+    let rings = analyzer::symbol_discovery(&config, &market);
     //
-    // Transform data :
+    // UPDATE PRICES
     //
-    let ignored_symbols:Vec<&str> = data_ignored_symbols.split(',').collect();
-    let bridges_symbols:Vec<&str> = data_bridge_symbols.split(',').collect();
-    let mut data_cache:Vec<SymbolPrice> = vec![];
-    
-    let rings = analyzer::symbol_discovery(&market, &ignored_symbols, &bridges_symbols, &mut data_cache);
-
-    // return;
-
-    //
-    // THREADS
-    //
-    let mut hunter_pool:Vec<JoinHandle<(Option<String>)>> = vec![];
-
-    for ring in rings {
-        thread::sleep(DELAY_INIT);
-        let thread_ring = ring.1.clone();
-        let thread = thread::spawn(|| { analyzer::order_chain(thread_ring) });
-        hunter_pool.push(thread);
-    }
-    //
-    // Joining all
-    //
-    let mut round_result = vec![];
-    for hunter in hunter_pool{
-        match hunter.join().unwrap() {
-            Some(name) => {
-                // make a list of ring:
-                round_result.push(name);
-                //
-                // Plan :
-                // We may rank the most profit one from each round,
-                // then execute the order ring as fast as possible.
-                // benchmark the delay + order book.
-                //
-                // Analyze > Ranking > Execute > Evaluate > Sleep > Repeat
-                //
-            },
-            None => {
-
-            }
-        }
-    }
-    thread::sleep(DELAY_ROUND);
-    //
-    // later, we may use signal to trigger this
-    //
-    println!("> Joining all threads.. <");
-    println!("=== This Round result ===");
-
-    for name in round_result {
-        println!("\"{}\"",name);
-    }
-
-    // ending
-    println!("Ended all threads.");
-
+    analyzer::init_threads(rings);
 }
 
